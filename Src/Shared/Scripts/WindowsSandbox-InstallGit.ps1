ba0ae60288f7bea5389389e0df781d452c5eb750
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2025-05-04 09:53:42 -0300

    Adding script parameters and imports; Removing line-by-line log code

--- Src/Shared/Scripts/WindowsSandbox-InstallGit.ps1
+++ Src/Shared/Scripts/WindowsSandbox-InstallGit.ps1
@@ -3,54 +3,40 @@ param (
     [string] $ScriptRootPath,
     
     [Parameter(Mandatory = $true, HelpMessage = "Caminho raiz do projeto onde será realizado todo o processo.")]
-    [string] $ProjectPath,
-
-    [Parameter(Mandatory = $true, HelpMessage = "Caminho padrão para a gravação de logs gerados.")]
-    [string] $DefaultLogPath,
-    
-    [Parameter(Mandatory = $false, HelpMessage = "Caminho para a cópia de logs gerados no caminho padrão.")]
-    [string] $LogPath = $null
+    [string] $ProjectPath
 )
 
-Import-Module "$ScriptRootPath/scripts/System/Modules/WindowsSandbox-SandboxCommonScripts.psm1"
-Import-Module "$ScriptRootPath/scripts/System/Modules/WindowsSandbox-BinaryHandling.psm1"
-Import-Module "$ScriptRootPath/scripts/Shared/Modules/WindowsSandbox-GitModule.psm1"
+Import-Module "$ScriptRootPath/Src/System/Modules/WindowsSandbox-CommonScripts.psm1"
+Import-Module "$ScriptRootPath/Src/System/Modules/WindowsSandbox-BinaryHandling.psm1"
+Import-Module "$ScriptRootPath/Src/Shared/Modules/WindowsSandbox-GitModule.psm1"
 
 ###
 
-New-DefaultLogPath -DefaultLogPath $DefaultLogPath
-New-LogPath -LogPath $LogPath
-
 # 8. Obtendo a URL da última versão do Git via API do GitHub
 Write-Output "Definindo a última versão do Git."
-Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
-$URLInstalador = Get-GitReleaseVersionURL
+$InstallerURL = Get-GitReleaseVersionURL
 
 # 9. Definindo o Caminho completo do instalador
 $InstallerPath = "$ScriptRootPath/Output/Programs"
 # 5. Verificando diretório de instalação para criar o diretório caso necessário
 Write-Output "Verificando se o diretório do instalador existe."
-Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
-New-InstallerPath -InstallerPath $InstallerPath
 
+New-InstallerPath -InstallerPath $InstallerPath
 Write-Output "Caminho do instalador: $InstallerPath."
-Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
 
 # 10. Baixando o instalador do Git
-Write-Output "Baixando o Git mais recente..."
+Write-Output "Definindo o caminho do instalador Git"
 $InstallerFile = "$InstallerPath/GitInstaller.exe"
-Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
-Receive-Installer -InstallerURL $URLInstalador -InstallerPath $InstallerFile
+
+Write-Output "Baixando o Git mais recente..."
+Receive-Installer -InstallerURL $InstallerURL -InstallerPath $InstallerFile
 
 # 11. Instalando o Git em modo silencioso
 Write-Output "Instalando o Git..."
-Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
-
 Invoke-Executable -ExecutableFile $InstallerFile -ArgumentList @("/VERYSILENT") -Wait:$true
 
 # 12. Adicionando Git ao PATH e verificando instalação
 Write-Output "Adicionando Git ao PATH."
-Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
 $env:Path += ";$env:ProgramFiles\Git\bin;$env:ProgramFiles\Git\cmd"
 $env:ProgramFiles
 # 13. Vefificando se o git foi instalado corretamente
@@ -58,11 +44,9 @@ $env:ProgramFiles
 try {
     $GitVersion = & git --version | Out-String
     Write-Output "Versão do git: $GitVersion"
-    Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
 }
 catch {
     Write-Output "Erro: Git não foi instalado corretamente ou não está no PATH."
-    Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
 
     exit 1
 }
