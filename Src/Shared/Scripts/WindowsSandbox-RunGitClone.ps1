ff1952389d95321a0c6fb1165802d0e3f6656c0c
Allan de Oliveira Almeida <arumeidaaran@outlook.com>
2025-05-04 09:51:43 -0300

    Adding script parameters and imports; Removing line-by-line log code; Adding call to Get-GitRepositoryName function.

--- Src/Shared/Scripts/WindowsSandbox-RunGitClone.ps1
+++ Src/Shared/Scripts/WindowsSandbox-RunGitClone.ps1
@@ -1,55 +1,60 @@
+param (
+    [Parameter(Mandatory = $true, HelpMessage = "Caminho raiz do projeto onde deve conter os scripts de execução das funções internas.")]
+    [string] $ScriptRootPath,
+    
+    [Parameter(Mandatory = $true, HelpMessage = "Caminho onde será clonado o projeto.")]
+    [PSCustomObject] $ProjectPath,
+
+    [Parameter(Mandatory = $true, HelpMessage = "URL de clone para o projeto no git.")]
+    [PSCustomObject] $GitRepositoryUrl
+)
+
+### PIPELINE IMPORTS ###
+Import-Module -Name "$ScriptRootPath/Src/System/Modules/WindowsSandbox-CommonScripts.psm1" `
+    -Force `
+    -ErrorAction Stop
+
+Import-Module -Name "$ScriptRootPath/Src/Shared/Modules/WindowsSandbox-GitModule.psm1" `
+    -Force `
+    -ErrorAction Stop
+
+### PIPELINE SCRIPT ###
+
 # 15. Definindo o caminho do repositório a partir da URL + caminho do projeto
 Write-Output "Tratando o caminho do repositório."
-$NomeRepo = $RepoUrl -split '/' | Select-Object -Last 1
-$NomeRepo = $NomeRepo -replace '.git', ''
 
-$RepoPath = "$ProjectPath/$NomeRepo"
-Write-Output "Caminho do repositório: $RepoPath"
-Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
+$RepositoryName = Get-GitRepositoryName -GitRepositoryUrl $GitRepositoryUrl
+
+$RepositoryPath = "$ProjectPath/$RepositoryName"
+
+Write-Output "Caminho do repositório: $RepositoryPath"
 
 # 16. Removendo diretório de destino, se necessário
 Write-Output "Verificando se o diretório de destino já existe."
-Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
-if (Test-Path $RepoPath) {
-    Write-Output "Removendo diretório existente: $RepoPath"
-    Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
-    Remove-Item -Path $RepoPath -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
+if (Test-Path $RepositoryPath) {
+    Write-Output "Removendo diretório existente: $RepositoryPath"
+    Remove-Item -Path $RepositoryPath -Recurse -Force -ErrorAction SilentlyContinue | Out-Null
 
     Write-Output "Aguardando o diretório do repositório ser removido..."
-    Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
-
-    $WaitPathResult = Wait-Path -Path $RepoPath -TimeoutLimit 5 -Mode 'Exists'
+    $WaitPathResult = Wait-Path -Path $RepositoryPath -TimeoutLimit 5 -Mode 'NotExists'
     if (-not $WaitPathResult) {
         Write-Output "Erro: O diretório do repositório não foi removido."
-        Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
-
+        New-TempErrorFile -Path $LogPath -ErrorFileName "RepositoryPathWaitNotExists"
+    
         exit 1
     }
 }
 
 # 17. Criando o diretório do repositório
 Write-Output "Criando diretório do projeto..."
-Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
-New-Item -Path $RepoPath -ItemType 'Directory' -Force -ErrorAction SilentlyContinue | Out-Null
+New-Item -Path $RepositoryPath -ItemType 'Directory' -Force -ErrorAction SilentlyContinue | Out-Null
 
 # 18. Enquanto o caminho não existir ou 5 segundos aguardando
 Write-Output "Aguardando o diretório do repositório ser criado..."
-Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
-$Contagem = 0
-$ContagemLimite = 10
-while (
-    (-not (Test-Path -Path $RepoPath))-and
-    ($Contagem -lt $ContagemLimite)
-) {
-    Start-Sleep -Seconds 1
-    $contagem = $Contagem + 1
-    Write-Output "Tentativa $Contagem de $ContagemLimite."
-    Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
-}
-
-if ($Contagem -gt $ContagemLimite) {
+$WaitPathResult = Wait-Path -Path $RepositoryPath -TimeoutLimit 5 -Mode 'Exists'
+if (-not $WaitPathResult) {
     Write-Output "Erro: O diretório do repositório não foi criado."
-    Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
+    New-TempErrorFile -Path $LogPath -ErrorFileName "RepositoryPathWaitExists"
 
     exit 1
 }
@@ -57,14 +62,12 @@ if ($Contagem -gt $ContagemLimite) {
 # 19. Tenta clonar o repositório com captura detalhada de erros
 #   Se der erro por 5 vezes, para a execução logando o erro
 Write-Output "Clonando o repositório."
-Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
-$SaidaGitClone = git clone $RepoUrl $RepoPath 2>&1
+$SaidaGitClone = git clone $GitRepositoryUrl $RepositoryPath 2>&1
 if ($LASTEXITCODE -ne 0) {
     Write-Output $SaidaGitClone
     Write-Output "Abortando..."
-    Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
 
     exit 1
 }
-Write-Output "Repositório $RepoPath clonado com sucesso."
-Copy-LogToLogPath -DefaultLogPath $DefaultLogPath -LogPath $LogPath
+
+Write-Output "Repositório $RepositoryPath clonado com sucesso."
